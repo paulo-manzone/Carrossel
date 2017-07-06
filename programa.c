f181858ff26a48db7d94a9f4fc27cb1a01860d9d
asdasdasd <asd>
2017-07-06 19:31:16 -0300

    Implementado funcao de velocidade

--- programa.c
+++ programa.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
 
-int  main(){
-    printf("!oi");
+
+//=========================================
+//				INSTRUÇÕES
+//=========================================
+
+//-digitalWrite() coloca tensão baixa ou alta em algum pino
+//ex:  digitalWrite(PINO1,HIGH) coloca tensão alta no pino1
+//-setup() é chamado na inicilização do programa
+//-loop() é chamado sem parar pelo durante a execução, após a execução
+//de setup()
+//
+
+void setup(){
+	//Declarando variavel
+    int PINO1=1;
+    
+    //Declarando Pinos
+
+	  pinMode (PINO1, OUTPUT);
+	    
+	//Mensagem de inicialização
+	
+	system("cls");
+	printf("\nSistema inicializado!\n");
+}
+
+void loop(){
+
+}
+
+void setVelocidade(int velocidade, int pino){
+	if(velocidade>10){
+		return;
+	}
+	int j;
+	for(j=0; j<50; j++){
+		digitalWrite(pino, HIGH);
+		sleep(velocidade);
+		digitalWrite(pino, LOW;);
+	}
+	
 }
